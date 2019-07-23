3f32dfe734458c64544f7a14c8638f5afa93f5f1
Blane Downing-Jennings <u6025652@umail.utah.edu>
2019-07-23 11:47:48 -0600

    going to try to keep track of useful snippets & resources; init

---
+++ powershell/node_runner.ps1
@@ -0,0 +1,11 @@
+﻿function Run-Node([string]$Script,[string[]]$vals) {
+    node.exe $PSScriptRoot\$script $vals
+
+}
+# ex: Run-Node ../script.js arg_1,arg_2
+
+function Run-Python([string]$Script,[string[]]$vals) {
+    python.exe $PSScriptRoot\$script $vals
+
+}
+# ex: Run-Python script.py arg_1,arg_2
